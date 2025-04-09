cf46a832e73eb6a73f4ee9eee7fc0f3c20884f63
hamed18 <hamedcnec@gmail.com>
2025-04-09 06:27:34 +0600

    created user controllers, interface, model, validation and router

---
+++ bikezone-serverside/src/app/modules/user/user.controller.ts
@@ -0,0 +1,36 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { UserServices } from './user.service';
+
+const createCustomer = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body;
+
+  const result = await UserServices.createCustomerIntoDB(password, studentData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Customer is created succesfully',
+    data: result,
+  });
+});
+
+
+const createAdmin = catchAsync(async (req, res) => {
+  const { password, admin: adminData } = req.body;
+
+  const result = await UserServices.createAdminIntoDB(password, adminData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin is created succesfully',
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  createCustomer,
+  createAdmin,
+};
