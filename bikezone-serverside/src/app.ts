ede7acba36dc7dcf0b06899c26ff336edc0a9c0a
e <jannatul0040@gmail.com>
2025-04-09 15:19:22 +0600

    Fixed some issues

--- bikezone-serverside/src/app.ts
+++ bikezone-serverside/src/app.ts
@@ -3,18 +3,28 @@ import express, { Application, Request, Response } from "express";
 
 import { globalErrorHandler } from "./app/middleeatres/globalErrorHandler";
 import productRouter from "./app/modules/product/product.router";
-import orderRouter from './app/modules/order/order.router';
+import orderRouter from "./app/modules/order/order.router";
 
 const app: Application = express();
+//  CORS setup
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+    ],
+    credentials: true,
+  })
+);
 
 //parsers
 app.use(express.json());
-app.use(cors());
 
 // application routes
 
 app.use("/api", productRouter);
-app.use('/api/order', orderRouter)
+app.use("/api/order", orderRouter);
 
 const getAController = (req: Request, res: Response) => {
   res.send("Hello World!");
