cd89dcbc8abb117dcbe3d63e88812a0b0cdb960f
e <jannatul0040@gmail.com>
2025-04-09 22:14:27 +0600

    Modified Card design

--- bikezone-clientside/src/pages/home/Product/ProductCard.tsx
+++ bikezone-clientside/src/pages/home/Product/ProductCard.tsx
@@ -1,6 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { useGetAllProductsQuery } from "@/redux/features/product/productApi";
 import { TProduct } from "@/types";
-import { BiDollar } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const ProductCard = () => {
@@ -10,27 +10,53 @@ const ProductCard = () => {
     return <div>Loading...</div>;
   }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-2">
-      {data?.data?.slice(0, 6).map((product: TProduct) => (
-        <div className="shadow-xl" key={product._id}>
-          <div className=" h-40 flex justify-center items-center overflow-hidden">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="h-full object-contain"
-            />
-          </div>
+    <div>
+      {" "}
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-16">
+        {data?.data?.slice(0, 6).map((product: TProduct) => (
+          <div className=" max-w-md w-full shadow-md">
+            <div className="relative">
+              <img
+                alt="Banner Profile"
+                src={product.image}
+                className="w-full rounded-none h-52"
+              />
 
-          <h3 className="font-bold text-center text-ellipsis overflow-hidden  px-2">
-            {product.name}
-          </h3>
-          <p className="flex justify-center items-center font-semibold text-[#E81938] px-2">
-            {product.price}
-            <BiDollar />
-          </p>
-          <Link to={`/product/${product._id}`}>View Details</Link>
-        </div>
-      ))}
+              <div className="absolute bg-black text-base text-white flex justify-center items-center bottom-0 left-2/4 transform -translate-x-1/2 translate-y-1/2 w-20 h-20 rounded-full border-2 font-extrabold border-white">
+                ${product.price}
+              </div>
+            </div>
+
+            <div className="p-5">
+              <p className="text-gray-400 text-base mt-5 ">
+                In Stock :
+                <span className=" text-black font-bold">
+                  {" "}
+                  {product.quantity}
+                </span>
+              </p>
+              <Link to={`/product/${product._id}`}>
+                <p className="text-2xl font-medium mt-3 hover:underline">
+                  {product.name}
+                </p>
+              </Link>
+
+              <div className="flex justify-start gap-5 items-center py-2 font-bold text-gray-700">
+                <li className="list-inside  list-disc  text-base">
+                  Brand : {product.brand}
+                </li>
+                <p className=" text-base">Category : {product.category}</p>
+              </div>
+              <Button>Add to Cart</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center items-center mx-auto py-10">
+        <Link to="/products/">
+          <Button>View All</Button>
+        </Link>
+      </div>
     </div>
   );
 };
