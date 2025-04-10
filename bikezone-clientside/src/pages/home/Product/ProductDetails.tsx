6fa547ccedd45120d1582ab3bfcbf49e922c9eae
e <jannatul0040@gmail.com>
2025-04-10 15:29:10 +0600

    Added checkout button

--- bikezone-clientside/src/pages/home/Product/ProductDetails.tsx
+++ bikezone-clientside/src/pages/home/Product/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { useGetAllProductsQuery } from "@/redux/features/product/productApi";
 import { TProduct } from "@/types";
 import { useParams } from "react-router-dom";
@@ -32,12 +33,13 @@ const ProductDetails = () => {
           <h2 className="text-2xl text-[#E81938] font-bold border-b border-black mb-2">
             Price: ${product.price}
           </h2>
-          <div className="text-gray-700 font-semibold">
+          <div className="text-gray-700 font-semibold pb-4">
             <p className="">Category : {product.category}</p>
             <p className="my-2">Available : {product.quantity}</p>
             <p className="">Brand : {product.brand}</p>
             <p className="mt-4">{product.description}</p>
           </div>
+          <Button>Checkout</Button>
         </div>
       </div>
     </div>
