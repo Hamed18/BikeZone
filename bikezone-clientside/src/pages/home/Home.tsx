8e362566161f22a15b10f2fbfac246bfbb28d2d4
hamed18 <hamedcnec@gmail.com>
2025-04-10 16:50:30 +0600

    updated

--- bikezone-clientside/src/pages/home/Home.tsx
+++ bikezone-clientside/src/pages/home/Home.tsx
@@ -1,5 +1,4 @@
 import Banner from "./Banner";
-import Marquee from "./Marquee";
 import FeaturedProduct from "./Product/FeaturedProduct";
 import Scroll from "./Scroll/Scroll";
 import Testimonials from "./Testimonials/Testimonials";
