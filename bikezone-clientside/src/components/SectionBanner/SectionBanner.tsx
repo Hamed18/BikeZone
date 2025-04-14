bb872360565be4c0f741a83ac312d7384cb60106
e <jannatul0040@gmail.com>
2025-04-14 15:58:04 +0600

    Fixed some issues

--- bikezone-clientside/src/components/SectionBanner/SectionBanner.tsx
+++ bikezone-clientside/src/components/SectionBanner/SectionBanner.tsx
@@ -2,14 +2,21 @@ import { GoDotFill } from "react-icons/go";
 interface SectionBannerProps {
   heading: string;
   subHeading: string;
+  bannerImage: string;
 }
-const SectionBanner = ({ heading, subHeading }: SectionBannerProps) => {
+const SectionBanner = ({
+  heading,
+  subHeading,
+  bannerImage,
+}: SectionBannerProps) => {
+  const defaultImage =
+    "https://i.ibb.co.com/jk8CB45Z/photo-custom-made-retro-motorcycle-against-skyscraper.jpg";
   return (
     <div className="w-full">
       <div className="border relative">
         <img
           alt="banner"
-          src="https://i.ibb.co.com/jk8CB45Z/photo-custom-made-retro-motorcycle-against-skyscraper.jpg"
+          src={bannerImage || defaultImage}
           className="w-full  md:h-[60vh] object-cover"
         />
         <div className="absolute inset-0 bg-black/50 z-10" />
